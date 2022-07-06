58ec2ddcf92db802e58a57620d9517f501758c0d
Caleb Harrigan <caleb.b.harrigan@vanderbilt.edu>
2022-07-06 22:08:23 +0000

    functioning genius API

--- bestbuy.py
+++ bestbuy.py
@@ -1,14 +1,26 @@
 import requests
+import json
+def function1(x):
+	return x * 0
 
-url = "https://bestbuyraygorodskijv1.p.rapidapi.com/getAllCategories"
+def function2(x, y):
+	return (x + y)
 
-payload = "apiKey=%3CREQUIRED%3E"
+artist = input("input an Artist name: ")
+
+url = "https://genius.p.rapidapi.com/search"
+
+querystring = {"q":artist}
+
 headers = {
-	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-	"X-RapidAPI-Host": "BestBuyraygorodskijV1.p.rapidapi.com"
+	"X-RapidAPI-Key": "22251c60demsh8ba7718637f6696p162e43jsnd56247a114ef",
+	"X-RapidAPI-Host": "genius.p.rapidapi.com"
 }
 
-response = requests.request("POST", url, data=payload, headers=headers)
+response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+data = json.loads(response.text)['response']['hits']
+for item in data:
+        # Print the artist and title of each result
+    print(item['result']['primary_artist']['name']
+              + ': ' + item['result']['title'])

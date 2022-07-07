1a178cc1579d939e34d23a3391e2cecc614649f7
Velma Anyona <vanyona@u.rochester.edu>
2022-07-07 21:49:50 +0000

    Almost Final

--- Genius.py
+++ Genius.py
@@ -3,38 +3,40 @@ import json
 import sqlalchemy as db
 import pandas as pd
 
-def function1(x):
-	return x * 0
+def Genius():
+#cont = True
+	while True:	
 
-def function2(x, y):
-	return (x + y)
+		artist = input("input an Artist name: ")
 
-artist = input("input an Artist name: ")
+		url = "https://genius.p.rapidapi.com/search"
 
-url = "https://genius.p.rapidapi.com/search"
+		querystring = {"q":artist}
 
-querystring = {"q":artist}
+		headers = {
+			"X-RapidAPI-Key": "22251c60demsh8ba7718637f6696p162e43jsnd56247a114ef",
+			"X-RapidAPI-Host": "genius.p.rapidapi.com"
+		}
 
-headers = {
-	"X-RapidAPI-Key": "22251c60demsh8ba7718637f6696p162e43jsnd56247a114ef",
-	"X-RapidAPI-Host": "genius.p.rapidapi.com"
-}
+		response = requests.request("GET", url, headers=headers, params=querystring)
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+		data = json.loads(response.text)['response']['hits']
+		name = []
+		song_title = []
+		for item in data:
+			name.append(item['result']['primary_artist']['name'])
+			song_title.append(item['result']['title'])
 
-data = json.loads(response.text)['response']['hits']
-name = []
-song_title = []
-for item in data:
-        # Print the artist and title of each result
-#    print(item['result']['primary_artist']['name']
-#              + ': ' + item['result']['title'])
-	name.append(item['result']['primary_artist']['name'])
-	song_title.append(item['result']['title'])
+		derived_data = {'name': name, 'Song Title': song_title}
+		genius_info = pd.DataFrame(derived_data)
+		engine = db.create_engine('sqlite:///artist_hits.db')
+		genius_info.to_sql('artist_songs', con=engine, if_exists='replace', index=False)
+		query_result = engine.execute("SELECT * FROM artist_songs;").fetchall()
+		print(pd.DataFrame(query_result))
 
-derived_data = {'name': name, 'Song Title': song_title}
-genius_info = pd.DataFrame.from_dict(derived_data)
-engine = db.create_engine('sqlite:///artist_hits.db')
-genius_info.to_sql('artist_songs', con=engine, if_exists='replace', index=False)
-query_result = engine.execute("SELECT * FROM artist_songs;").fetchall()
-print(pd.DataFrame(query_result))
+		user_answer = input('Would you like to continue(Y/N)?: ')
+		if user_answer == 'N':
+			# stop the program when done
+			print('Thank you for using our genius API <_>!!')
+			break
+Genius()
